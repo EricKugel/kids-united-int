7df02551b27379446ccb2ce354d7f049b25701cc
EricKugel <erickugel713@gmail.com>
2023-09-05 23:36:14 -0500

    stupid html stupid br br br brrrr

--- pages/api/register.js
+++ pages/api/register.js
@@ -37,26 +37,26 @@ export default async function handler(req, res) {
 Hello ` +
         name +
         `,
-\n
+<br/>
 A heartfelt welcome to Kids United International! 🌍 We're excited to have you on board as a member of our dynamic youth-led community. 
-\n
+<br/>
 Here are a few ways you can dive right in and make the most of your Kids United experience by checking out our entirely youth-developed website:
-\n
+<br/>
 1. Share Your Insights: We encourage you to contribute to our blog section. Share your thoughts, experiences, and ideas on whatever your heart desires. Help us and others around the globe get to know what a day in your shoes looks like. Your voice matters!
-\n
+<br/>
 2. Discover Youth Stories: Get to know fellow members by exploring their profiles. Every youth has a unique story to tell. It's a fantastic way to connect and learn from one other.
-\n
+<br/>
 3. Reach Out and Connect: Don't hesitate to reach out to other members. Collaboration often starts with a simple "hello." Who knows what incredible projects you might create together or what friendships you might develop?
-\n
+<br/>
 4. Prioritize Mental Health: Take a moment to check out our mental health resources. Your well-being matters, and we're here to support you every step of the way.
-\n
+<br/>
 5. Upcoming Projects: Curious about what's next? Visit our upcoming projects page to stay in the loop. Your involvement and contributions make a significant impact! 
-\n\n
+<br/><br/>
 Don't forget to connect with us on Instagram @kidsunitedint for international stories and updates. 
-\n\n
+<br/><br/>
 Even the smallest actions can create ripples of change. We're eagerly anticipating the mark you'll leave on our global community!
-\n\n
-Best,\n
+<br/><br/>
+Best,<br/>
 Kids United International
 
       `
