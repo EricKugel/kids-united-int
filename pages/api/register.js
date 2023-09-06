ca13a28a90d1e1edf8ba3cf4a6766ef29430d7ec
EricKugel <erickugel713@gmail.com>
2023-09-05 23:27:57 -0500

    email

--- pages/api/register.js
+++ pages/api/register.js
@@ -29,34 +29,34 @@ export default async function handler(req, res) {
 
     sendMail(
       [user.email],
-      "Your new Kids United Int account",
+      "Welcome to Kids United International - Let's Make a Difference Together!",
       "Thanks for creating an account!",
       `
-      Subject: Welcome to Kids United International - Let's Make a Difference Together!
+      
 
 Hello ` +
         name +
         `,
-
+\n
 A heartfelt welcome to Kids United International! 🌍 We're excited to have you on board as a member of our dynamic youth-led community. 
-
+\n
 Here are a few ways you can dive right in and make the most of your Kids United experience by checking out our entirely youth-developed website:
-
+\n
 1. Share Your Insights: We encourage you to contribute to our blog section. Share your thoughts, experiences, and ideas on whatever your heart desires. Help us and others around the globe get to know what a day in your shoes looks like. Your voice matters!
-
+\n
 2. Discover Youth Stories: Get to know fellow members by exploring their profiles. Every youth has a unique story to tell. It's a fantastic way to connect and learn from one other.
-
+\n
 3. Reach Out and Connect: Don't hesitate to reach out to other members. Collaboration often starts with a simple "hello." Who knows what incredible projects you might create together or what friendships you might develop?
-
+\n
 4. Prioritize Mental Health: Take a moment to check out our mental health resources. Your well-being matters, and we're here to support you every step of the way.
-
+\n
 5. Upcoming Projects: Curious about what's next? Visit our upcoming projects page to stay in the loop. Your involvement and contributions make a significant impact! 
-
+\n\n
 Don't forget to connect with us on Instagram @kidsunitedint for international stories and updates. 
-
+\n\n
 Even the smallest actions can create ripples of change. We're eagerly anticipating the mark you'll leave on our global community!
-
-Best,
+\n\n
+Best,\n
 Kids United International
 
       `
