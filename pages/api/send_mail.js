1d828d4a7cad4de7fbe30c1539b55617b08c15a4
EricKugel <erickugel713@gmail.com>
2023-09-08 11:35:37 -0500

    Email system?

---
+++ pages/api/send_mail.js
@@ -0,0 +1,34 @@
+import { connectDB } from "../../helpers/mongohelper";
+import User from "../../models/user";
+import { sendMail } from "../../helpers/email";
+
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "./auth/[...nextauth]";
+
+export default async function handler(req, res) {
+  if (req.method == "POST") {
+    await connectDB();
+    const session = await getServerSession(req, res, authOptions);
+    if (
+      session &&
+      JSON.parse(
+        JSON.stringify(await User.findOne({ email: session.user.email }).lean())
+      ).admin
+    ) {
+      const body = req.body;
+      const { html, subject } = body;
+      const allUsers = JSON.parse(JSON.stringify(await User.find({}).lean()));
+
+      var allEmails = [];
+      // allUsers.map((user) => {
+      //   allEmails.push(user.email);
+      // });
+      allEmails = ["erickugel713@gmail.com", "7886601@rochesterschools.org"];
+
+      sendMail(allEmails, subject, html, html);
+      res.status(200).json({ message: "Success!" });
+    } else {
+      res.status(401).json({ message: "Not authorized" });
+    }
+  }
+}
---
+++ pages/email.js
@@ -0,0 +1,93 @@
+import { getSession } from "next-auth/react";
+import User from "../models/user";
+import Editor from "../components/Editor";
+
+import { connectDB } from "../helpers/mongohelper";
+
+import { useState, useEffect } from "react";
+
+import { toast } from "react-hot-toast";
+
+import { useRouter } from "next/navigation";
+
+const Email = ({ email }) => {
+  const [editorLoaded, setEditorLoaded] = useState(false);
+  const [data, setData] = useState("");
+  const [subject, setSubject] = useState("");
+
+  const router = useRouter();
+
+  useEffect(() => {
+    setEditorLoaded(true);
+  }, []);
+
+  return (
+    <>
+      <Editor
+        name="description"
+        onChange={(data) => {
+          setData(data);
+        }}
+        editorLoaded={editorLoaded}
+      />
+
+      <form>
+        <input
+          type="text"
+          placeholder="Subject"
+          value={subject}
+          onChange={(e) => setSubject(e.target.value)}
+        ></input>
+        <button
+          onClick={() => {
+            fetch("/api/send_mail", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({ subject, html: data }),
+            }).then((response) => {
+              if (response.ok) {
+                toast.success("Email sent!");
+                router.push("/");
+              } else {
+                toast.error("Something went wrong");
+              }
+            });
+          }}
+        >
+          Send!
+        </button>
+      </form>
+
+      {/* {JSON.stringify(data)} */}
+    </>
+  );
+};
+
+export const getServerSideProps = async (context) => {
+  const session = await getSession(context);
+  await connectDB();
+  if (
+    session &&
+    JSON.parse(
+      JSON.stringify(await User.findOne({ email: session.user.email }).lean())
+    ).admin
+  ) {
+    try {
+      const email = session.user.email;
+      return { props: { email } };
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  return {
+    redirect: {
+      destination: "/",
+      permanent: false,
+    },
+  };
+};
+
+export default Email;
